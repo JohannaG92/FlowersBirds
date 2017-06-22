759015eaa60d4750652f07281d9c572ee423520a
JohannaG92 <johanna.gustafsson@live.com>
2017-06-22 15:35:30 +0200

    Upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var data;
 var bird;
 var rightAnswer;
+var ansToggle = false;
 
-//responsiveVoice.setDefaultVoice("Swedish Female");
+responsiveVoice.setDefaultVoice("Swedish Female");
 d3.json("https://codepen.io/JohannaG92/pen/owWGxV?editors=0010.js",function(error,jsonData){
   data = jsonData;
   init();
@@ -11,29 +12,44 @@ d3.json("https://codepen.io/JohannaG92/pen/owWGxV?editors=0010.js",function(erro
 function init(){
 	document.getElementById("OKButton").addEventListener("click", getImg);
 	getImg();
+	document.getElementById("soundButton").addEventListener("click", playSound);
 }
 
 function getImg(){
-	if (document.getElementById("inputBox").value!="") {
+	if (ansToggle == true) {
 		var answer = document.getElementById("inputBox").value;
 		answer = answer.toLowerCase();
 		if (answer == rightAnswer) {
-			document.getElementById("rightans").innerHTML = "RÄTT!";
+			document.getElementById("facitContainer").innerHTML = "RÄTT!";
+			document.getElementById("facitContainer").style.color = "Green";
+			document.getElementById("inputBox").value="";
 		}
 		else{
-			document.getElementById("rightans").innerHTML = "FEL!";
+			document.getElementById("facitContainer").innerHTML = "FEL! Rätt svar: "+rightAnswer;
+			document.getElementById("facitContainer").style.color = "Red";
+			document.getElementById("inputBox").value="";
 		}
-	};
-	var nrOfBirds = data.length;
-	var randomIndex = Math.floor((Math.random() * nrOfBirds));
-	bird = data[randomIndex];
-	rightAnswer = bird.name;
-	rightAnswer = rightAnswer.toLowerCase();
-	document.getElementById("pressedId").innerHTML = rightAnswer;
-	document.getElementById("latinName").innerHTML = bird.latinname;
-        var img = document.getElementById("birdImage");
-    	img.setAttribute("src", bird.url);
-    	var msg = new SpeechSynthesisUtterance(rightAnswer);
-	window.speechSynthesis.speak(msg);
-	//responsiveVoice.speak(rightAnswer);
+		ansToggle = false;
+		document.getElementById("OKButton").innerHTML = "Nästa";
+	}
+	else{
+		document.getElementById("OKButton").innerHTML = "OK";
+		document.getElementById("facitContainer").innerHTML = "";
+		var nrOfBirds = data.length;
+		var randomIndex = Math.floor((Math.random() * nrOfBirds));
+		bird = data[randomIndex];
+		rightAnswer = bird.name;
+		rightAnswer = rightAnswer.toLowerCase();
+		document.getElementById("latinName").innerHTML = bird.latinname;
+	    var img = document.getElementById("birdImage");
+	    img.setAttribute("src", bird.url);
+	    //var msg = new SpeechSynthesisUtterance(rightAnswer);
+		//window.speechSynthesis.speak(msg);
+		ansToggle = true;
+	}
+	
+}
+
+function playSound(){
+	responsiveVoice.speak(rightAnswer);
 }
