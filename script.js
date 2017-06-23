1bae3b31bf4c6a6c3f165b79d97c7bf3c74680d2
JohannaG92 <johanna.gustafsson@live.com>
2017-06-23 12:27:12 +0200

    Update 22/6

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ d3.json("https://codepen.io/JohannaG92/pen/owWGxV?editors=0010.js",function(erro
   init();
 })
 
+//d3.json("https://codepen.io/JohannaG92/pen/RggOBB?editors=0010.js",function(error,jsonData){
+//  data = jsonData;
+//  init();
+//})
+
 function init(){
 	document.getElementById("OKButton").addEventListener("click", getImg);
 	getImg();
@@ -22,17 +27,16 @@ function getImg(){
 		if (answer == rightAnswer) {
 			document.getElementById("facitContainer").innerHTML = "RÄTT!";
 			document.getElementById("facitContainer").style.color = "Green";
-			document.getElementById("inputBox").value="";
 		}
 		else{
-			document.getElementById("facitContainer").innerHTML = "FEL! Rätt svar: "+rightAnswer;
+			document.getElementById("facitContainer").innerHTML = "Rätt svar: "+rightAnswer;
 			document.getElementById("facitContainer").style.color = "Red";
-			document.getElementById("inputBox").value="";
 		}
 		ansToggle = false;
 		document.getElementById("OKButton").innerHTML = "Nästa";
 	}
 	else{
+		document.getElementById("inputBox").value="";
 		document.getElementById("OKButton").innerHTML = "OK";
 		document.getElementById("facitContainer").innerHTML = "";
 		var nrOfBirds = data.length;
